986cd0b77d1858c5dda918d2dbb170bbf1af5215
Alex Brown <alexpybrown@gmail.com>
2023-03-09 16:02:12 +0000

    Submission ID and "Add Another" button

--- src/components/ResponsesReview.tsx
+++ src/components/ResponsesReview.tsx
@@ -2,6 +2,9 @@ import { Dict } from "@/types"
 import { useState } from "react"
 import { CSVLink } from "react-csv";
 import {BsArrowDownCircle, BsArrowRightCircle, BsArrowLeftShort} from "react-icons/bs"
+import ShortUniqueID from "short-unique-id"
+
+const uid = new ShortUniqueID({length:10, dictionary: 'alpha_upper'})
 
 interface ResponsesReviewProps {
     responses: Dict
@@ -14,6 +17,8 @@ export default function ResponsesReview({responses, showUnderlyingData, visible}
     const [showResponsesButtonContent, setShowResponsesButtonContent] = useState(
         [<>Show Responses <BsArrowRightCircle size={24} className="inline"/></>, 
          <>Hide Responses <BsArrowDownCircle size={24} className="inline"/></>, ])
+    const [submissionID, _] = useState(uid());
+    const [downloadClicked, setDownloadClicked] = useState(false)
 
     const responsesExport = [Object.keys(responses), Object.values(responses).map(v=>v.toString().replaceAll('"', '""'))]
     return (
@@ -24,16 +29,7 @@ export default function ResponsesReview({responses, showUnderlyingData, visible}
                 className="mx-4 p-2 border-2 border-slate-300 rounded overflow-hidden bg-slate-100  text-slate-700">
                 <p>
                     Many thanks for taking the time to respond to this study. You may review your answers below.<br />
-
-                    Please download your response as a csv, and email this using a secure NHS email address to&nbsp; 
-                    <a href='mailto:alexander.brown5@nhs.net?subject=ANEC' target='_blank'
-                    className="text-sky-800"> 
-                    Alex Brown
-                    </a>
-                </p>
-
-                <p className="pt-4 text-sm italic">
-                    This method ensures that no data is stored on any non-NHS servers and patient data is kept secure.
+                    If there are any errors, you may go back using the button at the bottom of the page to correct them. Do not use the browser back button.
                 </p>
             </div>
             
@@ -53,16 +49,42 @@ export default function ResponsesReview({responses, showUnderlyingData, visible}
                     </ul>
                 }
             </div>
+            <div className="mx-4 p-2 border-2 border-slate-300 rounded overflow-hidden bg-slate-100  text-slate-700">
+                <p>
+                    When you are ready to submit this case, please download your responses by clicking on the button below, and email this via a secure NHS email address to&nbsp; 
+                    <a href='mailto:alexander.brown5@nhs.net?subject=ANEC' target='_blank'
+                    className="text-sky-800"> 
+                    Alex Brown
+                    </a><br />
+                </p>
+                <p className="py-2">
+                    Your submission ID is <code className="bg-slate-900 text-slate-50 px-2 py-0.5 border-2 rounded-md">{submissionID}</code>. Please retain this for your records in case of further queries.
+                </p>
+
+                <p className="pt-4 text-sm italic">
+                    This method ensures that no data is stored on any non-NHS servers and patient data is kept secure.
+                </p>
+            </div>
             <button className="bg-slate-100 hover:bg-slate-300 text-slate-700 font-bold py-2 px-4 ml-4 border border-sky-700 rounded"
                     onClick={()=>showUnderlyingData(true)}>
                 <BsArrowLeftShort size={24} className="inline"/>
                 Go Back  
             </button>
-            <CSVLink data={responsesExport} filename='responses.csv'>
-                <button className="bg-sky-500 hover:bg-sky-700 text-white font-bold py-2 px-4 ml-2 border border-sky-700 rounded">
-                    Download CSV  
+            <CSVLink data={responsesExport} filename={submissionID}>
+                <button className="bg-sky-500 hover:bg-sky-700 text-white font-bold py-2 px-4 ml-2 border border-sky-700 rounded"
+                        onClick={()=>setDownloadClicked(true)}>
+                    Download Responses (as CSV)
                 </button>
             </CSVLink>
+            
+            <button className={`${downloadClicked? '' : 'disabled'}
+                               bg-slate-100 hover:bg-slate-300 text-slate-700 font-bold py-2 px-4 ml-2 
+                               border border-slate-700 rounded 
+                               disabled:cursor-not-allowed disabled:text-slate-400 disabled:border-slate-400`}
+                    disabled={!downloadClicked}
+                    onClick={()=>window.location.reload()}>
+                    Start Another Report
+            </button>
         </div>
     }
     </>
