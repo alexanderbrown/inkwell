fbeff11a88c6bd0b989836106f7e9595fbd737c3
Alex Brown <alexander.p.brown@ucl.ac.uk>
2025-05-29 13:17:50 +0100

    Hard code available studies for now

--- src/server/api/routers/study.ts
+++ src/server/api/routers/study.ts
@@ -1,54 +1,24 @@
-import { readdirSync, readFileSync } from "fs";
-import { basename } from "path";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import type { Study } from "~/types";
 
+import AESD from "~/server/studies/AESD.json"
 
-function loadStudy(filePath: string): Study {
-  const fileContent = readFileSync(filePath, "utf-8");
-  const study = JSON.parse(fileContent);
-  return study as Study;
-}
-
-const STUDY_BASE_PATH = "src/server/studies";
-const STUDY_FILE_EXTENSION = "json";
-
+const studies = [
+  { name: "AESD",
+    study: AESD as Study,
+}]
 export const studyRouter = createTRPCRouter({
   get: publicProcedure
   .input(z.object({ name: z.string().optional() }))
-  .query(async ({ input }) => {
+  .query(({ input }) => {
     if (!input.name) return undefined
-    const filePath = `${STUDY_BASE_PATH}/${input.name}.${STUDY_FILE_EXTENSION}`;
-    const study = loadStudy(filePath);
-    return study;
+    return studies.find((study) => study.name === input.name)?.study || undefined;
   }),
 
   list: publicProcedure
-  .query(async () => {
-    const files = readdirSync(STUDY_BASE_PATH);
-    const studies = files
-      .filter((file) => file.endsWith(`.${STUDY_FILE_EXTENSION}`))
-      .map((file) => basename(file, `.${STUDY_FILE_EXTENSION}`));
-
-    let studyDescriptions: {name: string, study?: Study}[] = []
-    for (const studyName of studies) {
-      const filePath = `${STUDY_BASE_PATH}/${studyName}.${STUDY_FILE_EXTENSION}`;
-      const study = loadStudy(filePath);
-      if (study) {
-        studyDescriptions.push({
-          name: studyName,
-          study
-        });
-      }
-      else {
-        studyDescriptions.push({
-          name: studyName,
-          study: undefined
-        });
-      }
-    }
-    return studyDescriptions;
+  .query(() => {
+    return studies
   }),
 });
